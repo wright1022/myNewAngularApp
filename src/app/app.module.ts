038981a08c92d886de71fa0fd58c472faadbd766
wright1022 <MichaelFWright42@gmail.com>
2021-05-13 19:24:52 -0500

    ngmodel to array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyHomeCComponent } from './my-home-c/my-home-c.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeCComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
